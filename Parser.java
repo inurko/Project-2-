cf52c3069c5212be2ccc4422f791cd4ed5632ab6
ilannurko <nurki20@wfu.edu>
2022-03-22 21:04:22 -0400

    code

--- Parser.java
+++ Parser.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Parser {
@@ -82,12 +83,82 @@ public class Parser {
         use readInt here. Remember 1 Integer = 4bytes,
         which enlarge the size of file
          */
+        int k = 0;
+        while(k < 6596){
+            System.out.println(k);
+                getBlock(0,k);
+                k += 512;}
+
     }
 
-    public void run(){
-
+    public ArrayList<Record> getBlock(int runNumber, int index){
+        ArrayList<Record> Block = new ArrayList<>();
+        allRuns.get(runNumber);
+        int length = runsInfo.get(runNumber).getRunLength();
+        if ( 8192 > (length - (index * 16))){
+            System.out.println("less than 512");
+            //go to end of the run
+            for (int i = 0; i < ((length - (index* 16)) / 16); i++){
+                Block.add(allRuns.get(runNumber).get(index + i));
+            }
+        } else {
+            for (int i = 0; i < 512; i++){
+                Block.add(allRuns.get(runNumber).get(index + i));
+            }
+        }
+        return Block;
     }
 }
+
+    public void merge(int StartRunNum,int EndRunNum)
+    {
+        ArrayList<ArrayList<Record>> blocks=new ArrayList<>();
+        ArrayList<Integer> position=new ArrayList<>();
+        int totalRecords=0;
+        for(int i=StartRunNum;i<EndRunNum;i++) {
+            totalRecords=totalRecords+runsInfo.get(i).getRunLength()/16;
+            blocks.add(getBlock(runsInfo.get(i).getStart(),runsInfo.get(i).getRunLength()));
+        }
+        ArrayList<Record> curBlock=new ArrayList<>();
+        for(int j=0;j<totalRecords;j++) {
+        if(j%512==0) {
+            printToFile(curBlock);
+        }
+        int holdNum=position.get(0);
+        if(position.get(0)+runsInfo.get(0).getStart()==runsInfo.get(0).getRunLength())
+        {
+           blocks.remove(0);
+        }
+        else if (position.get(0)==blocks.get(0).size()) {
+                getBlock(runsInfo.get(0).getStart()+j,runsInfo.get(0).getRunLength());
+                position.set(0,0);
+            }
+        else {
+            int qHold=0;
+            double min = blocks.get(0).get(position.get(0)).getKey();
+
+            for (int q = 1; q < blocks.size(); q++) {
+                if(position.get(q)+runsInfo.get(q).getStart()==runsInfo.get(q).getRunLength())
+                {
+                    blocks.remove(q);
+                }
+
+                else if (position.get(q)==blocks.get(q).size()) {
+                    getBlock(runsInfo.get(q).getStart()+j,runsInfo.get(q).getRunLength());
+                    position.set(q,0);
+                }
+                else if (blocks.get(q).get(position.get(q)).getKey() < min) {
+                    min = blocks.get(q).get(position.get(q)).getKey();
+                    qHold = q;
+                }
+            }
+            Record holdRec =new Record(blocks.get(qHold).get(position.get(qHold)).getWholeRecord());
+            curBlock.add(holdRec);
+        }
+    }
+    }
+}
+
 
 
 
