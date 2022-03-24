511f0b50e2b90725ed35b4d6ff9138e5bdcd57a6
ilannurko <nurki20@wfu.edu>
2022-03-24 19:53:27 -0400

    stuff

--- Parser.java
+++ Parser.java
@@ -21,11 +21,10 @@ public class Parser {
     public Parser() throws IOException, FileNotFoundException {
         mergeFile = new RandomAccessFile("Merge.bin", "rw");
         firstMergeFile = new RandomAccessFile("mergeFirst.bin", "rw");
-        HoldFIle= new RandomAccessFile("HoldFile.bin","rw");
         runsInfo = new ArrayList<MergeInfo>();
         allRuns = new ArrayList<>();
         next = true;
-        numMerges=runsInfo.size();
+        numMerges=runsInfo.size()/8+1;
     }
 
     public void parseFile(String fileToParse, String infoToParse)
@@ -91,8 +90,7 @@ public class Parser {
         run();
     }
 
-    public void parseFileUS(RandomAccessFile fileToParse, ArrayList<MergeInfo> mergeInfoArrayList,int mergeNum) {
-        runsInfo=mergeInfoArrayList;
+    public void parseFileUS(RandomAccessFile fileToParse, int mergeNum) {
         numMerges=mergeNum;
 
         // executes while there is a next run (at least always 2)
@@ -174,6 +172,7 @@ public class Parser {
     public void merge(int StartRunNum,int EndRunNum) {
         // makes array lists
         ArrayList<ArrayList<Record>> blocks = new ArrayList<>();
+        ArrayList<Record> curBlock = new ArrayList<>();
         // position in the blocks array
         ArrayList<Integer> position = new ArrayList<>();
         int qHold=0;
@@ -189,12 +188,7 @@ public class Parser {
             blocks.add(getBlock(i, runsInfo.get(i).getStart()));
         }
         System.out.println("total Records " + totalRecords);
-        ArrayList<Record> curBlock = new ArrayList<>();
         for (int j = 0; j < totalRecords; j++) {
-            if (j % 512 == 0) { //512 might b wrong bc j = 0
-                printToFile(curBlock);
-                curBlock.clear();
-            }
             boolean hasMin=false;
             Record min=null;
             for(int q= 0; q<blocks.size();q++)
@@ -202,33 +196,44 @@ public class Parser {
                 if(position.get(q)==runsInfo.get(q+StartRunNum).getRunLength()){
                     blocks.set(q,null);
                 }
-                else if(blocks.get(q)==null||blocks.get(q).size()==0)
+                System.out.println("blocks size " + blocks.get(q).size());
+                if(blocks.get(q)==null||blocks.get(q).size()==0)
                 {
                     q++;
                 }
-                else if(position.get(q)%512==0){
-                   blocks.set(q,getBlock(StartRunNum+q,position.get(q)));
-                }
-                else if(hasMin==false)
-                {
-
-                    min=new Record(blocks.get(q).get(position.get(q)%512-1).getWholeRecord());
-                    qHold=q;
-                    q++;
-                }
-                else if(hasMin==true&&blocks.get(q).get(position.get(q)%512-1).compareTo(min)==-1)
-                {
-                    min=new Record(blocks.get(q).get(position.get(q)%512-1).getWholeRecord());
-                    qHold=q;
+                else {
+                    if (position.get(q) % 512 == 0) {
+                        blocks.set(q, getBlock(StartRunNum + q, position.get(q)));
+                    }
+                    if (hasMin == false && !(blocks.get(q).size() == 0)) {
+                        System.out.println("Call1");
+                        System.out.println("blocks size "+blocks.get(q).size());
+                        min = new Record(blocks.get(q).get(position.get(q) % 512).getWholeRecord());
+                        hasMin = true;
+                        qHold = q;
+                        q++;
+                    }
+                    else if (hasMin == true && blocks.get(q).get(position.get(q) % 512).compareTo(min) == -1 && !(blocks.get(q).size() == 0)) {
+                        min = new Record(blocks.get(q).get(position.get(q) % 512).getWholeRecord());
+                        qHold = q;
+                    }
                 }
             }
             if(min!=null) {
+                System.out.println("Call2");
                 curBlock.add(min);
                 int HoldPos = position.get(qHold);
                 position.set(qHold, HoldPos + 1);
             }
+            if (j % 512 == 0) { //512 might b wrong bc j = 0
+                System.out.println("Hopefully it prints this"+j);
+                System.out.println("curBlock " + curBlock.size());
+                printToFile(curBlock);
+                curBlock.clear();
+            }
         }
-        if(!curBlock.isEmpty()){
+
+        if(!curBlock.isEmpty()) {
             printToFile(curBlock);
             curBlock.clear();
         }
@@ -237,47 +242,28 @@ public class Parser {
 
     public void printToFile(ArrayList<Record> FileToPrint){
         //How to print the first or just know when it is
-        if (numMerges%2==0){
-            try {
-                mergeFile = new RandomAccessFile("Merge.bin", "rw");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
-            for(int i=0;i<FileToPrint.size();i++)
+        System.out.println(FileToPrint.size());
+            for(int i = 0;i < FileToPrint.size(); i++)
             {
+                System.out.println(" Print");
                 try {
                     mergeFile.write(FileToPrint.get(i).getWholeRecord());
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             //Write to the mergeFile
-        }
-        else {
-            try {
-                HoldFIle= new RandomAccessFile("HoldFile.bin","rw");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            for(int i=0;i<FileToPrint.size();i++)
-            {
-                try {
-                    HoldFIle.write(FileToPrint.get(i).getWholeRecord());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            //write to holdFile
-        }
+
     }
 
 
     public void run(){
         // decides which file to write to
-
+        int countNumMerges = 0;
         // runs as long as runsInfo has more than one run
         while(runsInfo.size() > 1){
+            countNumMerges++;
             ArrayList<MergeInfo> runsInfoTwo = new ArrayList<MergeInfo>();
             // checks for groups of 8 runs
             int numOfMerges = runsInfo.size()/8;
@@ -304,8 +290,20 @@ public class Parser {
                 runsInfoTwo.add(new MergeInfo(runsInfo.get(numOfMerges*8).getStart(),newLen));
                 numOfMerges++;
             }
-
             runsInfo = runsInfoTwo;
+            if (countNumMerges ==1) {
+                //write to first
+            }
+            if(numMerges!=0){
+                if(numMerges%2==0){
+                    parseFileUS(mergeFile,numMerges-1);
+                    try {
+                        mergeFile = new RandomAccessFile("Merge.bin", "rw");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 }
